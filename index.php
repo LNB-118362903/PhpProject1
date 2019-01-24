5918092da4b61cc2c2a27bceb16541a4efdd875c
118332701 <118332701@BISB67VL.bis.ad.ucc.ie>
2019-01-24 09:48:37 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>my first php</title>
+    </head>
+    <body>
+        <?php
+        echo 'hello world'; //how to print hello world in php
+        ?>
+    </body>
+</html>
